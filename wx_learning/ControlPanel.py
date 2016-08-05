4646c8bc26f0e9b258c7825482222b618a671937
sbalagudas <allan.g.xu@nsn.com>
2016-08-05 17:00:38 +0800

    5 Aug

--- wx_learning/ControlPanel.py
+++ wx_learning/ControlPanel.py
@@ -1,16 +1,25 @@
 import wx
 
-BMP_SIZE = 16
-BMP_BORDER = 3
-NUM_COLS = 4
-SPACING = 4
+class ControlPanel(wx.Panel):
+    BMP_SIZE = 16
+    BMP_BORDER = 3
+    NUM_COLS = 4
+    SPACING = 4
 
-colorList=('Black','Yellow','Red','Green',
+    colorList=('Black','Yellow','Red','Green',
            'Cyan','Blue','Grey','Forest Green',
            'Spring Green','Purple','Pink','Light Blue',
            'Dark Green','Dark Grey','Navy','Goldenrod',
            'Brown','Aquamarine','Orange','Dim Grey',)
-maxThickness = 16
+    maxThickness = 16
+    def __init__(self,parent,ID,paint) :
+        wx.Panel.__init__(self,parent,ID,style=wx.RAISED_BORDER)
+        self.paint = paint
 
-class ControlPanel(wx.Panel):
-    def __init__(self) :
+        buttonSize = (self.BMP_SIZE + 2* self.BMP_BORDER,self.BMP_SIZE + 2* self.BMP_BORDER)
+        colorGrid =
+
+    def createColorGrid(self,parent,buttonSize):
+        self.colorMap = {}
+        self.colorButtons = {}
+        colorGrid = wx.GridSizer(cols=self.NUM_COLS,)

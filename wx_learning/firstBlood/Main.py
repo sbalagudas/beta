3c16316f825540077252dd16b543f2ca4aa794cf
sbalagudas <allan.g.xu@nsn.com>
2016-09-08 17:16:45 +0800

    start to add table grid to the main frame

--- wx_learning/firstBlood/Main.py
+++ wx_learning/firstBlood/Main.py
@@ -4,8 +4,15 @@ import timeDisplay
 import costPanel as cP
 import login
 import time
+import tableGrid
 
 class mainFrame(wx.Frame):
+    tableData = (('aa',11,'aaa','2016-01-01 11:11:11'),
+                          ('bb',11,'bbb','2016-01-02 11:11:22'),
+                          ('cc',11,'cccasdfasdfasdfa','2016-01-03 11:11:33'),
+                          ('dd',11,'dddasdfasdfasdfasdfas','2016-01-04 11:11:44'),
+                          ('ee',11,'eeeasdfasdf','2016-01-05 11:11:55'))
+    tableLabel = ('cost name','value','comments','date')
     def __init__(self,
                  parent=None,
                  id=wx.ID_ANY,
@@ -18,15 +25,16 @@ class mainFrame(wx.Frame):
         self.panelTime = timeDisplay.timeDisplay(self,-1,'apple')
         #self.panelPaint = pt(self,-1)
         self.cost = cP.costPanel(self,-1)
-        self.tstPanel = cP.testPanel(self,-1)
-        self.tstPanel.SetBackgroundColour('White')
-        self.layout(self.panelTime,self.cost,self.tstPanel)
+        #self.tstPanel = cP.testPanel(self,-1)
+        #self.tstPanel.SetBackgroundColour('White')
+        #self.tableGrid = tableGrid.SimpleGrid(self,self.tableData,self.tableLabel)
+        self.tableGrid = tableGrid.gridPanel(self,-1)
+        self.layout(self.panelTime,self.cost,self.tableGrid)
 
-
-    def layout(self,time,cP,tst):
+    def layout(self,time,cP,tableGrid):
         subBox = wx.BoxSizer(wx.HORIZONTAL)
         subBox.Add(cP,0)
-        subBox.Add(tst,1,wx.EXPAND)
+        subBox.Add(tableGrid,1,wx.EXPAND)
 
         mainBox = wx.BoxSizer(wx.VERTICAL)
         mainBox.Add(time,1,wx.EXPAND)
--- wx_learning/firstBlood/common.py
+++ wx_learning/firstBlood/common.py
@@ -1,13 +1,10 @@
 import wx
 import time
-
+import enDecryption as ed
+import DBOperation
 
 def createStaticTextControl(parent,textInfo,font):
     sizer = wx.FlexGridSizer(cols=2,hgap=6,vgap=6)
-    #horSizer = wx.BoxSizer(wx.HORIZONTAL)
-    #verSizer = wx.BoxSizer(wx.VERTICAL)
-
-    #textDict = {}
     textList = []
     for eachItem in textInfo:
         if 'static' in eachItem :
@@ -17,13 +14,30 @@ def createStaticTextControl(parent,textInfo,font):
         textList.append(text)
         text.SetFont(font)
         sizer.Add(text,0,wx.EXPAND|wx.ALL,10)
-
-        #textDict[text.GetId()] = eachItem
-
-    #sizer.AddMany(textList)
-    #self.paintWindow.SetSizer(sizer)
     return sizer,textList
 
 def getTimeAndWeek():
     itf = "%Y-%m-%d %H:%M:%S"
     return (time.strftime(itf,time.localtime()),time.strftime("%W"))
+
+def getAndConvert(sqlCommand):
+    dbo = DBOperation.DBOperation()
+    rawData = dbo.customizedFetch(sqlCommand)
+    print "raw data : ",rawData
+    gridData = []
+    gridLabel = []
+    for item in rawData :
+        item = list(item)
+        print "item :",item
+        for i in range(1,4):
+            print "item before : ",item[i]
+            item[i]= ed.enDecryption.decryption(item[i].strip())
+            print "item after : ",item[i]
+            gridData.append(item[1:3])
+            gridLabel.append(item[len(item)-1:])
+    print "gridData : ",gridData
+    print "gridLabel : ",gridLabel
+    return gridData,gridLabel
+
+
+
